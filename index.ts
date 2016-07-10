d957bf046fdfbaaff5c2b29e8267e7368c59f75e
Howard Tang <aychtang@gmail.com>
2016-07-10 17:18:27 +0200

    Typescriptify.

--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
 
-function padL (l, s)
+type State = number[] | string[];
+
+function padL (l : number, s : string) : string
 {
   while (s.length < l)
     s = "0" + s;
@@ -7,12 +9,12 @@ function padL (l, s)
   return s;
 }
 
-function binaryFromRule (i)
+function binaryFromRule (i : number) : string
 {
   return padL(8, i.toString(2));
 }
 
-function outcomes (r)
+function outcomes (r : string) : any
 {
   return { "111" : r[0]
          , "110" : r[1]
@@ -25,14 +27,13 @@ function outcomes (r)
          };
 }
 
-function survival (st, i, r)
+function survival (st : State, i : number, r : string) : number
 {
   var pattern = "" + (st[i - 1] || 0) + st[i] + (st[i + 1] || 0);
-
-  return outcomes(r)[pattern];
+  return +outcomes(r)[pattern];
 }
 
-function compute (st, r)
+function compute (st : State, r : number) : number[]
 {
   var newState = [];
 
@@ -41,3 +42,5 @@ function compute (st, r)
 
   return newState;
 }
+
+export { compute }
